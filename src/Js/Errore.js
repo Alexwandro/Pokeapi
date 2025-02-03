d5e041a01f6122927cba3af05f6561cbe7d567ec
Alexwandro <115219875+Alexwandro@users.noreply.github.com>
2025-02-03 06:47:34 +0100

    Add files via upload

--- src/Js/Errore.js
+++ src/Js/Errore.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import {useNavigate } from 'react-router-dom'
 import '../Layout/Errore.css'
 
 const Errore = () => {
+	let indietro = useNavigate()
 	return (
 		<div id='container'>
-			<div>Non è la pokeball che stavi cercando</div>
-			<img src='https://www.kindpng.com/picc/m/74-743336_global-link-question-question-mark-unknown-pokemon-hd.png' alt='???'></img>
+			<div>404</div>
+			<div>LA PAGINA È FUGGITA</div>
+			<div>
+				<img src='https://www.kindpng.com/picc/m/74-743336_global-link-question-question-mark-unknown-pokemon-hd.png' alt='???'></img>
+			</div>
+			<input type='button' value={'Torna alla home'} onClick={() => indietro('/')}></input>
 		</div>
 	)
 }
